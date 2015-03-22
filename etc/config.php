1acd21e8f1ab37e70b1f63075e4cb6a4922fe0f6
Mark Kimsal <metrofindings@gmail.com>
2015-03-22 19:12:18 -0400

    Initial commit.

---
+++ etc/config.php
@@ -0,0 +1,52 @@
+<?php
+
+$serverList = array(
+	array(
+		'file'=>'local/zmws/src/server_run.php',
+		'name'=>'server',
+		'flags'=> array(
+			'log-level'=>'E',
+			'client-port'=>'5555',
+			'worker-port'=>'5556',
+			'news-port'=>'5557'
+		)
+	),
+	array(
+		'file'=>'local/zmws/src/http_gateway.php',
+		'name'=>'gateway',
+		'flags'=> array(
+			'log-level'=>'I',
+			'client-port'=>'5555',
+			'http-port'=>'5580'
+		)
+	)
+);
+
+$workerList = array(
+	array(
+		'file'=>'workers/sleep.php',
+		//name manages the log name and pid name
+		'name'=>'sleep_a',
+		'flags'=> array(
+			'log-level'=>'E',
+			'frontend-port'=>'5555',
+			'backend-port'=>'5556',
+
+			//without this, the inherited DEMO would be used
+			'service-name' => 'SLEEP'
+		)
+	),
+	array(
+		'file'=>'workers/reverse.php',
+		//name manages the log name and pid name
+		'name'=>'rev_a',
+		'flags'=> array(
+			'log-level'=>'E',
+			'frontend-port'=>'5555',
+			'backend-port'=>'5556',
+
+			//without this, the inherited DEMO would be used
+			'service-name' => 'STR-REV'
+		)
+	)
+);
